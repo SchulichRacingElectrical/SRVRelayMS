b1bd571b201d98c2d10d3f2a323cc5b515aacb8e
Tijunel <justintijunel@gmail.com>
2020-02-03 18:00:58 -0700

    Small edits

--- network.py
+++ network.py
@@ -37,29 +37,22 @@ vehicleData = {
 
 class Network:
     listenAddress = "0.0.0.0"
-    listenPort = 8080
+    listenPort = 8000
 
     def startServer(self):
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # this is for easy starting/killing the app
         soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        print('Socket created')
-
         try:
             soc.bind((self.listenAddress, self.listenPort))
-            print('Socket bind complete')
         except socket.error as msg:
             import sys
             print('Bind failed. Error : ' + str(sys.exc_info()))
             print(msg)
             sys.exit()
-
         # Start listening on socket
         soc.listen(10)
         print('Socket now listening')
-
-        # this will make an infinite loop needed for
-        # not reseting server for every client
+        # This will make an infinite loop needed for not reseting server for every client
         while True:
             connection, addr = soc.accept()
             ip, port = str(addr[0]), str(addr[1])
@@ -76,6 +69,9 @@ class Network:
     def clientThread(self, connection, IP, PORT, MAX_BUFFER_SIZE=4096):
         while 1:
             data = connection.recv(MAX_BUFFER_SIZE)
+            import time
+            millis = int(round(time.time() * 1000))
+            print (millis)
             if not data:
                 break
             fmt = "<ffffffffffffffffffffddff"
