025fd4bbd5e625adc45d7ffd4eb6dd849ba09042
Tijunel <justintijunel@gmail.com>
2021-10-02 14:06:22 -0600

    Skeleton for sender

---
+++ server/sender.py
@@ -0,0 +1,26 @@
+from main import app
+
+class Sender:
+  sensors = []
+  def __init__(self):
+    # Make request to get sensors
+    pass
+
+  @app.route("/sensors/get_version")
+  def get_version():
+    # Return a version
+    pass
+
+  @app.route("/sensors/get_sensors")
+  def get_sensors():
+    # Return all sensors and send to the hardware
+    pass
+
+  @app.route("/sensors/get_diff")
+  def get_diff(version):
+    # Send the difference between version
+    pass
+
+  def send_message(self, message):
+    # Send message to the car that will be on the display
+    pass
