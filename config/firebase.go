8eada20f65937eed0c88bafd2bb53e70b01bf61b
Tijunel <justintijunel@gmail.com>
2021-11-17 21:33:31 -0700

    Added user endpoints

--- config/firebase.go
+++ config/firebase.go
@@ -7,36 +7,47 @@ import (
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
 	"google.golang.org/api/option"
 )
 
-type FirebaseDB struct {
+type Firebase struct {
 	Context context.Context
 	App     *firebase.App
+	Auth    *auth.Client
 	Client  *firestore.Client
 }
 
-func (f *FirebaseDB) FirestoreDBInit() error {
+func (f *Firebase) Init() error {
+	// Configure the context
 	f.Context = context.Background()
 	gopath := os.Getenv("GOPATH")
 	if gopath == "" {
 		gopath = build.Default.GOPATH
 	}
 
+	// Configure the app
 	sa := option.WithCredentialsFile(gopath + "/src/github.com/SchulichRacingElectrical/srv-database-ms/config/firebase_config.json")
 	app, err := firebase.NewApp(f.Context, nil, sa)
 	if err != nil {
 		return err
 	}
-
 	f.App = app
 
+	// Configure firestore
 	client, err := f.App.Firestore(f.Context)
 	if err != nil {
 		return err
 	}
-
 	f.Client = client
+
+	// Configure auth
+	auth, err := f.App.Auth(f.Context)
+	if err != nil {
+		return err
+	}
+	f.Auth = auth
+
 	// defer f.Client.Close() //TODO: close client later when server shutsdown
 
 	return nil
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database-ms/config"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -11,25 +12,30 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-var Db config.FirebaseDB
+var firebase config.Firebase
 
 func main() {
-	Db = config.FirebaseDB{}
-	err := Db.FirestoreDBInit()
-
+	// Configure firebase
+	firebase = config.Firebase{}
+	err := firebase.Init()
 	if err != nil {
 		fmt.Println("Could not connect to db:\n", err.Error())
 		return
 	}
 
+	// Configure routes
 	router := gin.Default()
 	databaseHandlers := router.Group("/database")
 	{
+		// GET
+		databaseHandlers.GET("/users/:organization_id")
+		databaseHandlers.GET("/users/:organization_id/:user_id", getUser)
 		databaseHandlers.GET("/sensors", getSensors)
 		databaseHandlers.GET("/sensors/:sid", getSensor)
-		//PUT
-		//DELETE
-		//POST
+		// POST
+		databaseHandlers.POST("/users", postUser)
+		// PUT
+		databaseHandlers.PUT("/users", putUser) // Should there be a param?
 	}
 
 	router.Run(":8080")
@@ -38,10 +44,10 @@ func main() {
 // TODO: move these to follow industry project layout
 
 func getSensors(c *gin.Context) {
-	dsnap := Db.Client.Collection("sensors")
+	dsnap := firebase.Client.Collection("sensors")
 
 	// TODO: check for last update queryparam
-	iter := dsnap.Documents(Db.Context)
+	iter := dsnap.Documents(firebase.Context)
 	sensors := make([]interface{}, 0)
 	for {
 		doc, err := iter.Next()
@@ -61,23 +67,22 @@ func getSensors(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"sensors": sensors,
 	})
-
 }
 
 func getSensor(c *gin.Context) {
 	sid := c.Param("sid")
 
-	dsnap, err := Db.Client.Collection("sensors").Doc(sid).Get(Db.Context)
+	dsnap, err := firebase.Client.Collection("sensors").Doc(sid).Get(firebase.Context)
 	if err != nil {
 		if status.Code(err) == codes.NotFound {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"message": "Sensor not found",
-				"error":   true,
+				"error": true,
 			})
 		} else {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"message": err.Error(),
-				"error":   true,
+				"error": true,
 			})
 		}
 		return
@@ -85,4 +90,127 @@ func getSensor(c *gin.Context) {
 
 	sensor := dsnap.Data()
 	c.JSON(http.StatusOK, sensor)
+}
+
+func getUsers(c *gin.Context) {
+	organization_id := c.Param("organization_id")
+
+	type user_struct struct {
+		user_id string
+		display_name string
+		role string
+	}
+
+	users := []user_struct{}
+
+	// Get all the user records
+	iter := firebase.Client.Collection("users").Where("organization_id", "==", organization_id).Documents(firebase.Context)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H {
+				"message": "Could not get users.",
+			})
+		}
+		
+		user, authError := firebase.Auth.GetUser(firebase.Context, doc.Ref.ID)
+		if authError != nil {
+			c.JSON(http.StatusInternalServerError, gin.H {
+				"message": "Could not get users.",
+			})
+		}
+
+		// Concatenate user and auth into list, then return
+		users = append(users, user_struct{
+			doc.Ref.ID,
+			user.DisplayName,
+			doc.Data()["role"].(string),
+		})
+	}
+
+	data,parseError := json.Marshal(users)
+	if parseError != nil {
+		c.JSON(http.StatusInternalServerError, gin.H {
+			"message": "Parsing error.",
+		})
+	}
+	c.JSON(http.StatusOK, data)
+}
+
+func getUser(c *gin.Context) {
+	user_id := c.Param("user_id")
+
+	snapshot, err := firebase.Client.Collection("users").Doc(user_id).Get(firebase.Context) 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H {
+			"message": "User not found",
+			"error": true,
+		})
+	}
+
+	user := snapshot.Data()
+	c.JSON(http.StatusOK, user)
+}
+
+func postUser(c *gin.Context) {
+	// Get body
+	type user_struct struct {
+		organization_id string
+		user_id string
+	}
+	decoder := json.NewDecoder(c.Request.Body)	
+	var user user_struct
+	decodeError := decoder.Decode(&user)
+	if decodeError != nil {
+		c.JSON(http.StatusBadRequest, gin.H {
+			"message": "Body is incorrectly formatted.",
+		})
+		return
+	}
+
+	// Create the user entry
+	_, fetchError := firebase.Client.Collection("users").Doc(user.user_id).Set(firebase.Context, map[string]interface{}{
+		"organization_id": user.organization_id,
+		"role": "member",
+	})
+	if fetchError != nil {
+		c.JSON(http.StatusInternalServerError, gin.H {
+			"message": "Could not create entry",
+			"error": true,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{}) // Don't want to return anything...
+}
+
+func putUser(c *gin.Context) {
+	// Get the body
+	type user_struct struct {
+		organization_id string
+		user_id string
+		role string
+	}
+	decoder := json.NewDecoder(c.Request.Body)
+	var user user_struct
+	decodeError := decoder.Decode(&user)
+	if decodeError != nil {
+		c.JSON(http.StatusBadRequest, gin.H {
+			"message": "Body is incorrectly formatted.",
+		})
+	}
+
+	// Update the user
+	_, fetchError := firebase.Client.Collection("users").Doc(user.user_id).Set(firebase.Context, c.Request.Body)
+	if fetchError != nil {
+		c.JSON(http.StatusInternalServerError, gin.H {
+			"message": "Could not update user.",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{})
 }
