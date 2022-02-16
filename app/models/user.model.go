268813298c8c3d721978aae25ba4a0c7e4489523
Arham Humayun <arham099@hotmail.com>
2022-02-16 16:29:37 -0700

    update user model to include password

--- app/models/user.model.go
+++ app/models/user.model.go
@@ -8,6 +8,7 @@ type User struct {
 	ID             bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
 	DisplayName    string        `json:"name,omitempty" bson:"name,omitempty"`
 	Email          string        `json:"email,omitempty" bson:"email,omitempty"`
+	Password       string        `json:"password,omitempty" bson:"password,omitempty"`
 	OrganizationId string        `json:"organizationId,omitempty" bson:"organizationId,omitempty"`
 	Roles          string        `json:"roles,omitempty" bson:"roles,omitempty"`
 }
