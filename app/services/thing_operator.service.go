046dbb8d52d90ceeb4a99921b55f71c0ae2b3005
Tijunel <justintijunel@gmail.com>
2022-04-27 13:41:48 -0600

    Added thing operator service functions

--- app/services/thing_operator.service.go
+++ app/services/thing_operator.service.go
@@ -7,6 +7,8 @@ import (
 	"database-ms/config"
 	"database-ms/databases"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"gopkg.in/mgo.v2"
 )
@@ -27,15 +29,28 @@ func NewThingOperatorService(db *mgo.Session, c *config.Configuration) ThingOper
 }
 
 func (service *ThingOperatorService) Create(ctx context.Context, thingOperator *model.ThingOperator) error {
-	return nil
+	_, err := service.ThingOperatorCollection(ctx).InsertOne(ctx, thingOperator)
+	return err
 }
 
 func (service *ThingOperatorService) Delete(ctx context.Context, thingId string, operatorId string) error {
-	return nil
+	bsonThingId, err := primitive.ObjectIDFromHex(thingId)
+	if err != nil {
+		return err
+	}
+	bsonOperatorId, err := primitive.ObjectIDFromHex(operatorId)
+	if err != nil {
+		return err
+	}
+	_, err = service.ThingOperatorCollection(ctx).DeleteOne(ctx, bson.M{"thingId": bsonThingId, "operatorId": bsonOperatorId})
+	return err
 }
 
-func (service *ThingOperatorService) IsAssociationUnique(ctx context.Context, thingOperator *model.ThingOperator) bool {
-	return false
+func (service *ThingOperatorService) IsAssociationUnique(ctx context.Context, newThingOperator *model.ThingOperator) bool {
+	var thingOperator model.ThingOperator
+	query := bson.M{"thingId": newThingOperator.ThingId, "operatorId": newThingOperator.OperatorId}
+	err := service.ThingOperatorCollection(ctx).FindOne(ctx, query).Decode(&thingOperator)
+	return err != nil
 }
 
 func (service *ThingOperatorService) ThingOperatorCollection(ctx context.Context) *mongo.Collection {
