21d2367490c90d9592cc2e859c837661452a3dd8
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-05-22 15:57:53 -0600

    handle creating subscribers and receiving data from redis

---
+++ redisHandler/handler.go
@@ -0,0 +1,78 @@
+package redisHandler
+
+import (
+	"context"
+	"database-ms/config"
+	"log"
+
+	"encoding/json"
+
+	"github.com/go-redis/redis/v8"
+	"gopkg.in/mgo.v2"
+)
+
+type Message struct {
+	Active  bool   `json:"active"`
+	ThingId string `json:"THING"`
+}
+
+func Initialize(conf *config.Configuration, dbSession *mgo.Session) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     conf.RedisUrl + ":" + conf.RedisPort,
+		Password: conf.RedisPassword,
+	})
+
+	go awaitThingDataSessions(redisClient)
+}
+
+func awaitThingDataSessions(redisClient *redis.Client) {
+	ctx := context.Background()
+	subscriber := redisClient.Subscribe(ctx, "THING_CONNECTION")
+
+	for {
+		msg, err := subscriber.ReceiveMessage(ctx)
+		if err != nil {
+			panic(err)
+		}
+		message := Message{}
+		json.Unmarshal([]byte(msg.Payload), &message)
+		if message.Active {
+			go thingDataSession(message.ThingId, redisClient)
+		}
+	}
+}
+
+func thingDataSession(thingId string, redisClient *redis.Client) {
+	ctx := context.Background()
+	subscriber := redisClient.Subscribe(ctx, "THING_"+thingId)
+	log.Println("Thing Data Session Started for " + thingId)
+
+	for {
+		msg, err := subscriber.ReceiveMessage(ctx)
+		if err != nil {
+			panic(err)
+		}
+		message := Message{}
+		json.Unmarshal([]byte(msg.Payload), &message)
+
+		if !message.Active {
+			// Get thing data from redis
+			thingData, err := redisClient.LRange(ctx, "THING_"+thingId, 0, -1).Result()
+			if err != nil {
+				panic(err)
+			}
+
+			// Delete thing data from redis
+			err = redisClient.Del(ctx, "THING_"+thingId).Err()
+			if err != nil {
+				panic(err)
+			}
+
+			// Parse thing data
+			log.Println(thingData)
+
+			// Save thing data to mongo
+			// Save thing data to csv
+		}
+	}
+}
