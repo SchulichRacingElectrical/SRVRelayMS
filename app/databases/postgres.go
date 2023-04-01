e99e5047c66b47529a86f043cad65ad8ee6c3eb7
Jonathan Breidfjord <jbreidfjord@gmail.com>
2023-04-01 14:25:44 -0600

    update compose to run postgres db and create db

--- app/databases/postgres.go
+++ app/databases/postgres.go
@@ -11,7 +11,37 @@ import (
 var postgresDB *gorm.DB
 
 func InitPostgres(config *config.Configuration) *gorm.DB {
-	dsn := fmt.Sprintf(
+	connectionString := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		config.Host,
+		config.User,
+		config.Password,
+		"postgres",
+		config.Port,
+		config.SslMode,
+	)
+
+	// Open connection to postgres
+	postgresDB, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Check if database exists
+	stmt := fmt.Sprintf("SELECT * FROM pg_database WHERE datname = '%s';", config.DbName)
+	rs := postgresDB.Raw(stmt)
+	if rs.Error != nil {
+		panic(rs.Error)
+	}
+
+	// Create database if not exists
+	var rec = make(map[string]interface{})
+	if rs.Find(rec); len(rec) == 0 {
+		postgresDB.Exec("CREATE DATABASE " + config.DbName)
+	}
+
+	// Get new connection to database
+	connectionString = fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		config.Host,
 		config.User,
@@ -20,7 +50,7 @@ func InitPostgres(config *config.Configuration) *gorm.DB {
 		config.Port,
 		config.SslMode,
 	)
-	postgresDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	postgresDB, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
